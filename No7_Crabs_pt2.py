94ab0ffc787bd7ba7babdaa26d4c528a15f621e1
GammaProteo <87981743+GammaProteo@users.noreply.github.com>
2021-12-07 19:41:07 +0100

    Big numero 6 pt 2 ... čurákův triangl + opraveny Stehlikovy chyby v zapisu

--- No7_Crabs_pt2.py
+++ No7_Crabs_pt2.py
@@ -15,13 +15,12 @@ max_crab = max(crab_cords)
 
 for step in range(0, max_crab):
     for crab in crab_cords:
-        fuel += ((abs(crab - step) ** 2 + abs(crab - step)) // 2)
-
+        fuel += ((abs(crab - step) ** 2 + abs(crab - step)) // 2)  # Pascaluv triangl (triangular number),
+                                                                   # vypocet rozvoje pro n-ty krok + spotreba paliva
+                                                                   # ** - na druhou, // celociselne deleni
     if fuel < min_fuel:
         min_fuel = fuel
     fuel = 0
-
-
 
 print(crab_cords)
 print(min_fuel)
