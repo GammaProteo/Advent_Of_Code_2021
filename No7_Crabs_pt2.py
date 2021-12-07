7083dba59f58802eeb4dbaae78c8611773ed98ef
GammaProteo <87981743+GammaProteo@users.noreply.github.com>
2021-12-07 19:38:17 +0100

    Big numero 6 pt 2 ... čurákův triangl

---
+++ No7_Crabs_pt2.py
@@ -0,0 +1,27 @@
+import sys
+
+with open('Data/Crabs.data') as f:  # loading txt file from data, reading each line
+    temp_cords = f.read().split(',')
+    f.close()
+
+# crab_cords = []
+# for crab in range(0, len(temp_cords)):
+#     crab_cords.append(int(temp_cords[crab]))
+
+crab_cords = [int(x) for x in temp_cords]  # pitonic vej # fagt pycha # tvoje mama
+fuel = 0
+min_fuel = sys.maxsize
+max_crab = max(crab_cords)
+
+for step in range(0, max_crab):
+    for crab in crab_cords:
+        fuel += ((abs(crab - step) ** 2 + abs(crab - step)) // 2)
+
+    if fuel < min_fuel:
+        min_fuel = fuel
+    fuel = 0
+
+
+
+print(crab_cords)
+print(min_fuel)
